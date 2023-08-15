f335e3b18fc40b8870edeffdcd07be9c8bdf1e52
CyrilChi <chiyongmail@163.com>
2023-08-15 14:09:42 +0800

    Extract time calculation class

---
+++ timeClass.js
@@ -0,0 +1,48 @@
+/*
+ * @Author: CyrilChi chiyongmail@163.com
+ * @Date: 2023-08-15 11:27:30
+ * @LastEditors: CyrilChi chiyongmail@163.com
+ * @LastEditTime: 2023-08-15 13:57:19
+ * @FilePath: /24-h-clock/index.js
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
+export class InitTime {
+  constructor(time) {
+    this.time = time;
+  }
+  getCurrentTime() {
+    const year = this.time.getFullYear();
+    const month = this.time.getMonth() + 1;
+    const day = this.time.getDate();
+    const week = this.time.getDay();
+    const hours = this.time.getHours();
+    const minutes = this.time.getMinutes();
+    const seconds = this.time.getSeconds();
+    const hour = hours + minutes / 60;
+    const minute = minutes + seconds / 60;
+    console.log({ y: year, m: month, d: day, w: week, hs: hours, ms: minutes, h: hour, mm: minute, s: seconds });
+    return { y: year, m: month, d: day, w: week, hs: hours, ms: minutes, h: hour, mm: minute, s: seconds };
+  }
+}
+export class SunTime {
+  constructor(time) {
+    this.time = time;
+  }
+  getSunTime() {
+    // 日出时间计算
+    let sunrise = calcnew(1, 39.9, 116.4, this.time.y, this.time.m, this.time.d);
+    let sunset = calcnew(2, 39.9, 116.4, this.time.y, this.time.m, this.time.d);
+    let sunriseHours = sunrise.getHours();
+    let sunriseMinutes = sunrise.getMinutes();
+    let sunsetHours = sunset.getHours();
+    let sunsetMinutes = sunset.getMinutes();
+    return {
+      sunriseHours: sunriseHours,
+      sunriseMinutes: sunriseMinutes,
+      sunsetHours: sunsetHours,
+      sunsetMinutes: sunsetMinutes
+    };
+  }
+}
